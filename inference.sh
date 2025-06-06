301b56fd71cbfd460698d700698e21db2371ca03
htsuruo <tsuru.dev@gmail.com>
2025-06-06 22:57:30 +0900

    chore: Update some params

--- inference.sh
+++ inference.sh
@@ -1,4 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 uv run inference.py \
 	--dataset vitonhd \
 	--data_root_path ./VITON-HD \
-	--output_dir output
+	--output_dir output \
+	--batch_size 32 \
+	--dataloader_num_workers 8 \
+	--mixed_precision fp16
